d4b6cd19d1d8edf6b154c0e25355691300ac29ff
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2022-11-10 13:59:30 -0600

    Clean up layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,69 +1,18 @@
 import './globals.css'
-import Image from 'next/image';
-import Link from 'next/link';
 import Nav from '../components/navbar'
+import Foot from '../components/footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Brent Wilson - Huntsville, AL</title>
-        <meta name="description" content="The Wilson Net was created by Brent Wilson to showcase his life." />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+      <head />
       <body>
-        <div>
-          <Nav />
-          <main id="page" className="container mx-auto w-full px-4">
-            {children}
-          </main>
-          <footer className="footer p-10 bg-neutral text-neutral-content md:justify-evenly justify-center">
-            <div>
-              <Image
-                className="mask mask-squircle mx-auto"
-                alt="Brent Wilson"
-                src="/me.png"
-                width={120}
-                height={120}
-              />
-              <p className="w-32 text-center">
-                <span className="font-bold"> The Wilson Net</span>
-                <br />
-                Huntsville, AL
-              </p>
-            </div>
-
-            <div>
-              <span className="footer-title">Services</span>
-              <a className="link link-hover">Web Hosting</a>
-              <a className="link link-hover">WordPress</a>
-              <a className="link link-hover">XenForo</a>
-              <a className="link link-hover">Other</a>
-            </div>
-            <div>
-              <span className="footer-title">About Me</span>
-              <a className="link link-hover">Biography</a>
-              <Link
-                href="/about/resume"
-                title="Brent Wilson's Resume"
-                className="link link-hover"
-              >
-                Resume
-              </Link>
-              <a className="link link-hover">Projects</a>
-              <a className="link link-hover">Services</a>
-            </div>
-            <div>
-              <span className="footer-title">Misc</span>
-              <a className="link link-hover">Contact</a>
-              <a className="link link-hover">About This Website</a>
-              <a className="link link-hover">Terms of Use</a>
-              <a className="link link-hover">Privacy Policy</a>
-            </div>
-          </footer>
-        </div>
-      </body >
-    </html >
+        <Nav />
+        <main id="page" className="container mx-auto w-full px-4">
+          {children}
+        </main>
+        <Foot />
+      </body>
+    </html>
   )
 }
