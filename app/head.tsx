22cf2b95ce10dde2a4a4980db9c21f28cc85126f
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2022-11-10 13:59:17 -0600

    Add initial head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Brent Wilson - Huntsville, AL</title>
+      <meta name="description" content="The Wilson Net was created by Brent Wilson to showcase his life." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
