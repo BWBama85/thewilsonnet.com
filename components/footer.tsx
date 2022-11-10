22cce63a610d2e5cbb3ad7bc72b71d78f28b5466
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2022-11-10 14:00:32 -0600

    readd footer

---
+++ components/footer.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Foot() {
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content md:justify-evenly justify-center">
+      <Image
+        className="mask mask-squircle mx-auto"
+        alt="Brent Wilson"
+        src="/me.png"
+        width={120}
+        height={120}
+      />
+      <p className="w-32 text-center">
+        <span className="font-bold"> The Wilson Net</span>
+        <br />
+        Huntsville, AL
+      </p>
+
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Web Hosting</a>
+        <a className="link link-hover">WordPress</a>
+        <a className="link link-hover">XenForo</a>
+        <a className="link link-hover">Other</a>
+      </div>
+      <div>
+        <span className="footer-title">About Me</span>
+        <a className="link link-hover">Biography</a>
+        <Link
+          href="/about/resume"
+          title="Brent Wilson's Resume"
+          className="link link-hover"
+        >
+          Resume
+        </Link>
+        <a className="link link-hover">Projects</a>
+        <a className="link link-hover">Services</a>
+      </div>
+      <div>
+        <span className="footer-title">Misc</span>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">About This Website</a>
+        <a className="link link-hover">Terms of Use</a>
+        <a className="link link-hover">Privacy Policy</a>
+      </div>
+    </footer>
+  )
+}
