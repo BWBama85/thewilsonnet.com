8f824dbe4cc8ccd6b557de3884b38c5f3948d201
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2022-11-19 14:05:09 -0600

    Fix button size for mobile

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -5,27 +5,33 @@ export default function Nav() {
     <nav className="bg-primary text-primary-content lg:absolute lg:top-0 lg:right-0 lg:left-auto lg:bottom-auto lg:z-10 lg:rounded-bl-lg lg:rounded-br-none">
       <div className="flex justify-center lg:text-xl">
         <div>
-          <Link className="btn-primary btn lg:rounded-bl-lg xl:btn-lg" href="/">
+          <Link
+            className="btn-primary btn-sm btn lg:rounded-bl-lg xl:btn-lg"
+            href="/"
+          >
             Home
           </Link>
         </div>
         <div>
-          <Link className="btn-primary btn xl:btn-lg" href="/about">
+          <Link className="btn-primary btn-sm btn xl:btn-lg" href="/about">
             About
           </Link>
         </div>
         <div>
-          <Link className="btn-primary btn xl:btn-lg" href="/about/resume">
+          <Link
+            className="btn-primary btn-sm btn xl:btn-lg"
+            href="/about/resume"
+          >
             Resume
           </Link>
         </div>
         <div>
-          <Link className="btn-primary btn xl:btn-lg" href="/projects">
+          <Link className="btn-primary btn-sm btn xl:btn-lg" href="/projects">
             Projects
           </Link>
         </div>
         <div>
-          <Link className="btn-primary btn xl:btn-lg" href="/contact">
+          <Link className="btn-primary btn-sm btn xl:btn-lg" href="/contact">
             Contact
           </Link>
         </div>
