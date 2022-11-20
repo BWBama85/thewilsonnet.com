9618921c7188b0b1b0218c8f953ce230abfaaf26
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2022-11-19 19:57:49 -0600

    Add Mustang Evolution project

---
+++ app/projects/mustangevolution/page.tsx
@@ -0,0 +1,180 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function BamaPolitics() {
+  return (
+    <article className="prose mx-4 max-w-none rounded-3xl bg-base-200 p-4 lg:relative lg:mx-0 lg:w-full">
+      <h1>Bama Politics</h1>
+      <div className="divider"></div>
+      <Image
+        src="/mustangevolution_logo.svg"
+        className="mx-auto rounded-2xl lg:float-left lg:ml-0 lg:mr-4"
+        alt="Mustang Evolution Logo"
+        width={258}
+        height={170}
+        priority
+      />
+      <h2>History & Information</h2>
+      <p>
+        <Link
+          href="https://www.mustangevolution.com"
+          target="_blank"
+          title="Mustang Evolution"
+          className="link"
+        >
+          MustangEvolution.com
+        </Link>{' '}
+        was created by me in May of 2003, originally named 232stangs.com. I was
+        17 years old at the time, nearing the end of my Junior High School year.
+        For my 16th birthday I received a 1995 V6 Mustang and that, combined
+        with my love for computers, put me on the path that would create Mustang
+        Evolution.
+      </p>
+      <p>
+        Wanting to modify my Mustang to make it look and perform the way I
+        wanted it to, I joined my first online Ford Mustang forum soon after my
+        16th birthday. Curios to how the forums worked, I decided to try find a
+        way to make my own forum and learn the technologies behind them.
+      </p>
+      <p>
+        I bought the domain 232stangs.com,{' '}
+        <Link
+          href="https://web.archive.org/web/20030726013910/http://www.232stangs.com/"
+          title="232Stangs.com in 2003"
+          target="_blank"
+          className="link"
+        >
+          put up a landing page
+        </Link>{' '}
+        and then{' '}
+        <Link
+          href="https://web.archive.org/web/20030812005424/http://www.232stangs.net/"
+          title="232Stangs.net Forum"
+          target="_blank"
+          className="link"
+        >
+          created the forum
+        </Link>{' '}
+        using the free phpBB forum software. From there I was hooked. Now I
+        didn't want to just customize my Mustang, I wanted to customize my
+        Mustang Forum.
+      </p>
+      <h2>The Move To Mustang Evolution</h2>
+      <p>
+        After a few domain changes, I decided I wanted to do more than just be a
+        forum for V6 Mustang owners. The niche was too small and I didn't want
+        to lock myself into it, especially if one day they stopped making V6
+        Mustangs. While sitting in my room trying to think about what domain I
+        wanted to buy for a forum that included all Mustangs, I looked over at
+        my wall and saw a poster I had gotten for Christmas that showed the
+        Evolution of the Mustang with the words Mustang Evolution.
+      </p>
+      <p>
+        Once I had the domain, it was all about how I am going to grow the
+        forum. Free software was no longer going to cut it, so I migrated the
+        forum to vBulletin. Shared hosting wasn't reliable, so I bought my first
+        dedicated server and learned how to setup a LAMP server. Now that the
+        site was on reliable hosting, had reliable software, the rest of it was
+        hitting the ground marketing the new website.
+      </p>
+      <h3>Growth</h3>
+      <p>
+        Growing Mustang Evolution was now my mission. I didn't just want to be a
+        small website, I wanted to be THE website for Ford Mustang owners.
+      </p>
+      <h4>Meet & Greets</h4>
+      <p>
+        One of the effective ways of growing Mustang Evolution was driving my
+        Mustang to events or even hosting the events. Throughout my ownership of
+        Mustang Evolution I attended Mustang car shows and events all over the
+        country, marketing my website during the process. That process built
+        brand loyalty and in the process I made life long friends.
+      </p>
+      <h4>Search Engines</h4>
+      <p>
+        While Meet & Greets worked, people still weren't finding my website
+        searching on Google. I knew that I had to improve my SEO if I didn't
+        want to be left behind so I started following all the SEO blogs I could
+        find, reading every article I could get my hands on to learn the best
+        practices.
+      </p>
+      <p>
+        I also became active within the vBulletin community, looking for ways to
+        improve the software I was using. vBulletin was great at the time but
+        slow to adopt new technologies, so I found myself learning the vBulletin
+        code and modifying it myself to get the results I needed to improve the
+        sites performance in terms of both speed and SEO.
+      </p>
+      <h4>Making Money</h4>
+      <p>
+        While in college Google Adsense was starting to blow up and I started to
+        see how many people were now making money off their visitors by just
+        adding a few lines of JavaScript to their website. Up until this point,
+        I had been grinding it out by selling advertising spots on the website
+        directly to companies that sold parts or services for the Ford Mustang.
+        While this was nice, the money was not enough for my to do this full
+        time. However, the day I rolled Adsense out on the website, I realized
+        just how much money I had been leaving on the table. Now the reality of
+        owning my own business and working for myself was within reach.
+      </p>
+      <h2>Going Full Time</h2>
+      <p>
+        At the beginning of my third year of college I made a decision to leave
+        school and work on the website full time. I was making enough money to
+        support myself but felt like the time I was spending in college was
+        leaving money on the table with Mustang Evoltuion.
+      </p>
+      <p>
+        From that point on there was no looking back. I traveled across the
+        country working on building the Mustang Evolution brand, I attended
+        conferences related to building websites and online communities, and
+        even went to Washington D.C. to lobby for the online advertising
+        industry with the help of IAB, the Interactive Advertising Bureau.
+      </p>
+      <h2>Giving Up Half</h2>
+      <p>
+        Through my networking I discovered more people like me. People who were
+        building online web properties and specifically online communities. One
+        of those connections was{' '}
+        <Link
+          href="https://www.linkedin.com/in/robinowitz/"
+          title="Andy Robinowitz LinkedIn"
+          target="_blank"
+          className="link"
+        >
+          Andy Robinowitz
+        </Link>{' '}
+        the owner of{' '}
+        <Link
+          href="http://www.socialknowledge.com/"
+          title="Social Knowledge, LLC"
+          target="_blank"
+          className="link"
+        >
+          Social Knowledge, LLC
+        </Link>
+        . After some discussions, I sold 50 percent of Mustang Evolution to Andy
+        and went to work with him at Social Knowledge helping with his
+        communities as well as continuing to work on Mustang Evolution.
+      </p>
+      <p>
+        This lasted until April 2012, where I made the tough decision to sell
+        the remaining 50 percent. While it was hard to say goodbye to something
+        I built and cared about, I was excited about my new venture with another
+        business friend doing the same thing, building online communities.
+      </p>
+      <p>
+        That lead me to becoming a part of Group Net, LLC, running some of the
+        largest online communities on the internet, specifically
+        <Link
+          href="/projects/christianforums"
+          title="Christian Forums Project"
+          className="link"
+        >
+          ChristianForums.com
+        </Link>
+        .
+      </p>
+    </article>
+  );
+}
