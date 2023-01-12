7c01f01f6e57bb0c4da0dccb3cffd7693f20bdfd
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2023-01-12 02:45:17 -0600

    Add about component

---
+++ app/components/about.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function ProjectsComp() {
+  return (
+    <div className="not-prose hero bg-base-100">
+      <div className="hero-content text-center">
+        <div>
+          <h2 className="text-5xl font-bold">About Brent Wilson</h2>
+          <p className="py-6">
+            Brent Wilson is a highly skilled Webmaster, Developer, and Linux
+            System Administrator with a passion for technology, sports, animals,
+            and politics. He was born and raised in Huntsville, Alabama, but has
+            also lived in Dallas, Texas, and Chattanooga, Tennessee.
+          </p>
+          <Link href="/about" title="Projects" className="btn-primary btn">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
