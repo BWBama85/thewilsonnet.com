ec99672ffe9502ee9340ce82365c3da4c13a6eff
Brent Wilson <brent@thewilsonnet.com>
2023-06-30 00:06:38 -0500

    Clean up contact form

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -1,76 +1,91 @@
-import nodemailer from 'nodemailer';
+import nodemailer, {
+  SendMailOptions,
+  SentMessageInfo,
+  Transporter,
+} from 'nodemailer';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+function getEnvVariable(name: string) {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`${name} is not set`);
+  }
+  return value;
+}
+
+async function verifyTransporter(
+  transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo>
+): Promise<SMTPTransport> {
+  return new Promise((resolve, reject) => {
+    transporter.verify((error) => {
+      if (error) {
+        console.error(error);
+        reject(new Error('Transporter verification failed'));
+      } else {
+        close();
+      }
+    });
+  });
+}
+
+async function sendMail(
+  transporter: Transporter<SentMessageInfo>,
+  mailData: SendMailOptions
+) {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(new Error('Sending mail failed'));
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let accessKeyId;
-  let secretAccessKey;
-  let region;
-  let cfSecret;
+  try {
+    const accessKeyId = getEnvVariable('AWS_SES_ACCESS_KEY_ID');
+    const secretAccessKey = getEnvVariable('AWS_SES_SECRET_ACCESS_KEY');
+    const region = getEnvVariable('AWS_SES_REGION');
+    const cfSecret = getEnvVariable('CLOUDFLARE_TURNSTILE_SECRET_KEY');
+    const smtpPort = getEnvVariable('SMTP_PORT');
 
-  if (process.env.AWS_SES_ACCESS_KEY_ID) {
-    accessKeyId = process.env.AWS_SES_ACCESS_KEY_ID;
-  } else {
-    throw new Error('AWS_ACCESS_KEY is not set');
-  }
+    const { name, email, subject, message, token } = req.body;
+    const ip = req.headers['x-forwarded-for'] as string;
 
-  if (process.env.AWS_SES_SECRET_ACCESS_KEY) {
-    secretAccessKey = process.env.AWS_SES_SECRET_ACCESS_KEY;
-  } else {
-    throw new Error('AWS_SES_SECRET_ACCESS_KEY is not set');
-  }
+    const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
 
-  if (process.env.AWS_SES_REGION) {
-    region = process.env.AWS_SES_REGION;
-  } else {
-    throw new Error('AWS_SES_REGION is not set');
-  }
-
-  if (process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY) {
-    cfSecret = process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY;
-  } else {
-    throw new Error('Cloudflare Turnstile Secret Key Not Set');
-  }
-
-  const { name, email, subject, message, token } = req.body;
-  const ip = req.headers['x-forwarded-for'] as string;
-
-  const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-
-  const transporter = nodemailer.createTransport({
-    host: region,
-    port: 465,
-    secure: true, // upgrade later with STARTTLS
-    auth: {
-      user: accessKeyId,
-      pass: secretAccessKey,
-    },
-  });
-
-  const formData = new FormData();
-  formData.append('secret', cfSecret);
-  formData.append('response', token);
-  formData.append('remoteip', ip);
-  console.log(formData);
-
-  const result = await fetch(url, {
-    body: formData,
-    method: 'POST',
-  });
-
-  const outcome = await result.json();
-
-  if (outcome.success) {
-    await new Promise((resolve, reject) => {
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log('Server is ready to take our message');
-          resolve(success);
-        }
-      });
+    const transporter = nodemailer.createTransport({
+      host: region,
+      port: Number(smtpPort),
+      secure: true,
+      auth: {
+        user: accessKeyId,
+        pass: secretAccessKey,
+      },
     });
+
+    const formData = new FormData();
+    formData.append('secret', cfSecret);
+    formData.append('response', token);
+    formData.append('remoteip', ip);
+
+    const result = await fetch(url, {
+      body: formData,
+      method: 'POST',
+    });
+
+    const outcome = await result.json();
+
+    if (!outcome.success) {
+      return res.status(result.status).json(outcome);
+    }
+
+    await verifyTransporter(transporter);
 
     const mailData = {
       from: '"The Wilson Net" <brent@thewilsonnet.com>',
@@ -81,19 +96,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             <p>${message}</p>`,
     };
 
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(info);
-          resolve(info);
-        }
-      });
-    });
-    return res.status(200).json({ error: '' });
-  } else {
-    return res.status(result.status).json(outcome);
+    await sendMail(transporter, mailData);
+
+    return res.status(200).json({ success: 'Email was sent' });
+  } catch (err: unknown) {
+    console.error(err);
   }
 };
